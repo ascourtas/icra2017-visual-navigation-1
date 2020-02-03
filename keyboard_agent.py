588f027527615eba53e7454f9b46f0eb75244b2e
Zane <zanedenmon2020@u.northwestern.edu>
2020-02-02 21:30:35 -0600

    modifies keyboard_agent.py to use new ai2thor environment

--- keyboard_agent.py
+++ keyboard_agent.py
@@ -7,8 +7,9 @@ import signal
 import argparse
 import numpy as np
 
-from scene_loader import THORDiscreteEnvironment
+# from scene_loader import THORDiscreteEnvironment
 from utils.tools import SimpleImageViewer
+import ai2thor.controller
 
 #
 # Navigate the scene using your keyboard
@@ -17,20 +18,30 @@ from utils.tools import SimpleImageViewer
 def key_press(key, mod):
 
   global human_agent_action, human_wants_restart, stop_requested
-  if key == ord('R') or key == ord('r'): # r/R
-    human_wants_restart = True
-  if key == ord('Q') or key == ord('q'): # q/Q
-    stop_requested = True
-  if key == 0xFF52: # up
-    human_agent_action = 0
-  if key == 0xFF53: # right
-    human_agent_action = 1
-  if key == 0xFF51: # left
-    human_agent_action = 2
-  if key == 0xFF54: # down
-    human_agent_action = 3
 
-def rollout(env):
+  # Browser actions
+  if key == ord('R') or key == ord('r'): # r/R - Restart
+    human_wants_restart = True
+  if key == ord('F') or key == ord('f'): # f/F - Quit
+    stop_requested = True
+
+  # Agent actions
+  if key == 119 or key == 0xFF52: # w/W - Up
+    human_agent_action = "MoveAhead"
+  if key == 100 or key == 0xFF53: # d/D - Right
+    human_agent_action = "MoveRight"
+  if key == 97 or key == 0xFF51: # a/A - Left
+    human_agent_action = "MoveLeft"
+  if key == 115 or key == 0xFF54: # s/S - Down
+    human_agent_action = "MoveBack"
+
+  # Camera actions
+  if key == 101: # e/E - Rotate Camera Right
+    human_agent_action = "RotateRight"
+  if key == 113: # q/Q - Rotate Camera Left
+    human_agent_action = "RotateLeft"
+
+def rollout(event, controller, viewer):
 
   global human_agent_action, human_wants_restart, stop_requested
   human_agent_action = None
@@ -39,54 +50,42 @@ def rollout(env):
     # waiting for keyboard input
     if human_agent_action is not None:
       # move actions
-      env.step(human_agent_action)
+      event = controller.step(action=human_agent_action)
       human_agent_action = None
 
     # waiting for reset command
     if human_wants_restart:
       # reset agent to random location
-      env.reset()
+      controller.reset(scene='FloorPlan227')
       human_wants_restart = False
 
     # check collision
-    if env.collided:
+    if event.metadata['collided']:
       print('Collision occurs.')
-      env.collided = False
+      event.collided = False
 
     # check quit command
     if stop_requested: break
 
-    viewer.imshow(env.observation)
+    viewer.imshow(event.frame)
 
 if __name__ == '__main__':
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument("-s", "--scene_dump", type=str, default="./data/bedroom_04.h5",
-                      help="path to a hdf5 scene dump file")
-  args = parser.parse_args()
-
-  print("Loading scene dump {}".format(args.scene_dump))
-  env = THORDiscreteEnvironment({
-    'h5_file_path': args.scene_dump
-  })
-
-  # manually disable terminal states
-  env.terminals = np.zeros_like(env.terminals)
-  env.terminal_states, = np.where(env.terminals)
-  env.reset()
+  controller = ai2thor.controller.Controller(scene='FloorPlan227', gridSize=0.25, width=1000, height=1000);
+  event = controller.step(action='Pass') # Perform no action
 
   human_agent_action = None
   human_wants_restart = False
   stop_requested = False
 
   viewer = SimpleImageViewer()
-  viewer.imshow(env.observation)
+  viewer.imshow(event.frame)
   viewer.window.on_key_press = key_press
 
-  print("Use arrow keys to move the agent.")
+  print("Use WASD keys to move the agent.")
   print("Press R to reset agent\'s location.")
-  print("Press Q to quit.")
+  print("Press F to quit.")
 
-  rollout(env)
+  rollout(event, controller, viewer)
 
   print("Goodbye.")
