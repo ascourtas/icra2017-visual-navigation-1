9e41564e7205cab657fab18a1219787fc6c80f06
Aristana Scourtas <ascourtas@gmail.com>
2020-02-06 12:22:43 -0600

    update bot agent to programatically capture the image of the goal object

--- bot_agent.py
+++ bot_agent.py
@@ -1,16 +1,57 @@
 import ai2thor.controller
 import pprint
+import cv2
+import numpy as np
 
-controller = ai2thor.controller.Controller(scene='FloorPlan28', gridSize=0.25, width=1000, height=1000)
-print("before controller")
-event = controller.step(dict(action='MoveAhead'))
-print("bob")
-# Numpy Array - shape (width, height, channels), channels are in RGB order
-event.frame
-print("susan")
-# Numpy Array in BGR order suitable for use with OpenCV
-image = event.cv2image()
-print("karen")
+
+# TODO: account for rotation as well (like with floor lamp)
+def get_goal_position(goal_object_position, reachable_positions):
+    """
+    Find the reachable position closest to our goal object position
+
+    :param goal_object_position:
+    :param reachable_positions:
+    :return final_pos:
+    """
+
+    final_pos = {}
+    for coord_str in ['x', 'y', 'z']: # TODO: don't actually have to break them up, can just use whole positions
+        # reset the min difference per coordinate
+        min_pos_diff = np.inf
+        for rp in reachable_positions:
+            pos_diff = abs(goal_object_position[coord_str] - rp[coord_str])
+            # if this difference is less than the min, update the min
+            if pos_diff < min_pos_diff:
+                # update the coordinate to with the closer coord
+                final_pos[coord_str] = rp[coord_str]
+                min_pos_diff = pos_diff
+
+    return final_pos
+
+
+# the floor plan names correspond to those in constants.py
+controller = ai2thor.controller.Controller(scene='FloorPlan227', gridSize=0.25, width=1000, height=1000)
+
+# TV position is {'x': -3.503, 'y': 1.506, 'z': 0.001}
+goal_object_position = {'x': -3.503, 'y': 1.506, 'z': 0.001}
+# Floor lamp position is {'x': -0.369, 'y': 0.026053369, 'z': 4.992}, rotation: {'x': 0.0, 'y': 59.9998856, 'z': 0.0}
+# goal_object_position = {'x': -0.369, 'y': 0.026053369, 'z': 4.992}
+
+# get the positions that the agent could possibly reach
+event = controller.step(action='GetReachablePositions')
+reachable_positions = event.metadata['reachablePositions']
+
+# get the reachable position closest to our goal object
+final_pos = get_goal_position(goal_object_position, reachable_positions)
+event = controller.step(dict(action='Teleport', x=final_pos['x'], y=final_pos['y'], z=final_pos['z']))
+
+# # Numpy Array - shape (width, height, channels), channels are in RGB order
+# current_image = event.frame
+
+# # Numpy Array in BGR order suitable for use with OpenCV
+current_image = event.cv2image()
+cv2.imwrite("FP227_goal_TV.png", current_image)
+
 # current metadata dictionary that includes the state of the scene
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(event.metadata)
