ad33b4d9706baab1671242227383e68f9546cef8
Aristana Scourtas <ascourtas@gmail.com>
2020-02-06 12:26:26 -0600

    update bot agent to put screenshot in right place, and dynamically alter filename

--- bot_agent.py
+++ bot_agent.py
@@ -13,7 +13,6 @@ def get_goal_position(goal_object_position, reachable_positions):
     :param reachable_positions:
     :return final_pos:
     """
-
     final_pos = {}
     for coord_str in ['x', 'y', 'z']: # TODO: don't actually have to break them up, can just use whole positions
         # reset the min difference per coordinate
@@ -32,17 +31,17 @@ def get_goal_position(goal_object_position, reachable_positions):
 # the floor plan names correspond to those in constants.py
 controller = ai2thor.controller.Controller(scene='FloorPlan227', gridSize=0.25, width=1000, height=1000)
 
-# TV position is {'x': -3.503, 'y': 1.506, 'z': 0.001}
-goal_object_position = {'x': -3.503, 'y': 1.506, 'z': 0.001}
-# Floor lamp position is {'x': -0.369, 'y': 0.026053369, 'z': 4.992}, rotation: {'x': 0.0, 'y': 59.9998856, 'z': 0.0}
-# goal_object_position = {'x': -0.369, 'y': 0.026053369, 'z': 4.992}
+# # TV position is {'x': -3.503, 'y': 1.506, 'z': 0.001}
+goal_object = {"name": "TV", "position": {'x': -3.503, 'y': 1.506, 'z': 0.001}}
+# # Floor lamp position is {'x': -0.369, 'y': 0.026053369, 'z': 4.992}, rotation: {'x': 0.0, 'y': 59.9998856, 'z': 0.0}
+# goal_object_position = {"name": "FloorLamp", "position": {'x': -0.369, 'y': 0.026053369, 'z': 4.992}}
 
 # get the positions that the agent could possibly reach
 event = controller.step(action='GetReachablePositions')
 reachable_positions = event.metadata['reachablePositions']
 
 # get the reachable position closest to our goal object
-final_pos = get_goal_position(goal_object_position, reachable_positions)
+final_pos = get_goal_position(goal_object['position'], reachable_positions)
 event = controller.step(dict(action='Teleport', x=final_pos['x'], y=final_pos['y'], z=final_pos['z']))
 
 # # Numpy Array - shape (width, height, channels), channels are in RGB order
@@ -50,7 +49,7 @@ event = controller.step(dict(action='Teleport', x=final_pos['x'], y=final_pos['y
 
 # # Numpy Array in BGR order suitable for use with OpenCV
 current_image = event.cv2image()
-cv2.imwrite("FP227_goal_TV.png", current_image)
+cv2.imwrite("data/FP227_goal_{}.png".format(goal_object['name']), current_image)
 
 # current metadata dictionary that includes the state of the scene
 pp = pprint.PrettyPrinter(indent=4)
